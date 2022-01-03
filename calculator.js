48303fa351d1638f184976528d368e47488d4dfc
Amram1997 <amramananyan9@gmail.com>
2022-01-03 21:40:07 +0400

    feat calculator function constractor and resolved

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function Calculator(){
+ 
+}
+
+Calculator.prototype.add = function(number1,number2){
+  return number1 + number2
+}
+Calculator.prototype.subtract = function(number1,number2){
+  return number1 - number2
+}
+Calculator.prototype.multiply = function(number1,number2){
+  return number1 * number2
+}
+Calculator.prototype.divide = function(number1,number2){
+  return number1 / number2
+}
+const calculator = new Calculator();
+console.log(calculator.add(10,5));
+console.log(calculator.subtract(10, 5));
+console.log(calculator.multiply(10, 5));
+console.log(calculator.divide(10, 5));
